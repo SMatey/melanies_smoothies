e6bc1a34a61f69af2d5cdd892f97481588b8fb81
Sebastian Matey <101902474+SMatey@users.noreply.github.com>
2025-08-28 17:53:09 -0600

    Fix typo in use_container_width parameter

--- streamit_app.py
+++ streamit_app.py
@@ -55,7 +55,7 @@ if ingredients_list :
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_wwidth=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
 
