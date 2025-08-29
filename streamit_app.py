2575215bb52cf716605d2387ad6b6508fe14c63a
Sebastian Matey <101902474+SMatey@users.noreply.github.com>
2025-08-28 18:40:28 -0600

    Comment out dataframe display in streamit_app.py
    Comment out dataframe display and stop calls

--- streamit_app.py
+++ streamit_app.py
@@ -25,8 +25,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 
 #----------convertimos el snowflake dataframe a pandas dataframe
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 #------------Multiselect
 ingredients_list = st.multiselect (
     'Choose up 5 ingredients: '
